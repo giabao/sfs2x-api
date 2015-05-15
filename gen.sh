10bc5fecb38167180b9116ae2c846b2ca0cefdc4
giabao <giabao@sandinh.net>
2015-05-15 21:02:48 +0700

    haxelib v1.2.5

--- gen.sh
+++ gen.sh
@@ -1,1 +1,6 @@
+rm -Rf hxclasses
 haxe -debug -swf nothing.swf --no-output -swf-lib SFS2X_API_AS3.swc -swf-version 11.3 --gen-hx-classes  --macro patchTypes\(\'sfs.patch\'\)
+rm sfs2x-api.zip
+rm -Rf com && mkdir -p com/smartfoxserver
+mv hxclasses/com/smartfoxserver com/smartfoxserver
+zip -r sfs2x-api.zip . -icom/smartfoxserver/* -ihaxelib.json
